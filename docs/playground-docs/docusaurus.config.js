9b2e7519d826126c167a49b2db33fb846b646316
Shervin <shervinox@gmail.com>
2022-10-28 15:41:33 +0200

    test pipeline AFTER 2

--- docs/playground-docs/docusaurus.config.js
+++ docs/playground-docs/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Inner Site',
-  tagline: 'Inner Dinosaurs are cool',
+  title: 'My Other Site',
+  tagline: 'Other Dinosaurs are cool',
   url: 'https://github.com',
   baseUrl: '/playground/',
   onBrokenLinks: 'throw',
